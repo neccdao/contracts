a298c66226ca0b487686146f52752df0415bdcf7
Eric Juta <ericjohnjuta@gmail.com>
2021-12-11 02:03:09 +0000

    feat: 🎸 deploy
    to rinkeby

--- deploy/01-deploy-necc-diamond-rinkeby.js
+++ deploy/01-deploy-necc-diamond-rinkeby.js
@@ -352,7 +352,7 @@ async function deployNecc(hre) {
   await approvalTx.wait(1);
   console.log("sNecc approve staking");
 
-  // const nNeccD = await contractAt("nNeccFacet", nNecc.address);
+  const nNeccD = await contractAt("nNeccFacet", nNecc.address);
 
   let ndolBalance = await ndol.balanceOf(deployer.address);
   console.log(ndolBalance?.toString());
